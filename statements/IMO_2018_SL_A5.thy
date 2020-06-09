5dcde4e5b76418844cb4f7f26e912558cf34a8e8
Filip Marić <filip@matf.bg.ac.rs>
2020-06-09 18:53:36 +0200

    Several problem statements

---
+++ statements/IMO_2018_SL_A5.thy
@@ -0,0 +1,11 @@
+theory IMO_2018_SL_A5
+imports Complex_Main
+begin
+
+theorem
+  fixes f :: "real \<Rightarrow> real"
+  assumes "\<forall> x > 0. \<forall> y > 0. (x + 1/x) * (f y) = f (x*y) + f (y / x)"
+  shows "\<exists> C1 C2. \<forall> x > 0. f x = C1 * x + C2 / x"
+  sorry
+
+end
---
+++ statements/IMO_2018_SL_A7.thy
@@ -0,0 +1,10 @@
+theory IMO_2018_SL_A7
+imports Complex_Main
+begin
+
+theorem
+  shows "Max {root 3 (a / (b + 7)) + root 3 (b / (c + 7)) + root 3 (c / (d + 7)) + root 3 (d / (a + 7)) 
+              | a b c d :: real . a \<ge> 0 \<and> b \<ge> 0 \<and> c \<ge> 0 \<and> d \<ge> 0 \<and> a + b + c + d = 100} = 8 / root 3 7"
+  sorry
+
+end
